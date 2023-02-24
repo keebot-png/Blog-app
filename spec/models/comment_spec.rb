7a022ff7af187d2771c94077715904060a6d57cc
keebot-png <kee.bot@icloud.com>
2023-02-24 13:23:41 +0200

    Add tests for likes and comments methods and validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new(name: 'Keenan Botes', bio: 'Lives in South Africa', posts_counter: 5)
+    @post = Post.new(author: @user, title: 'Best blog ever', text: 'Lives in South Africa', comments_counter: 5,
+                     likes_counter: 17)
+    @comment = Comment.new(author: @user, post: @post, text: 'Loving this holiday trip')
+  end
+
+  describe '#comments_count' do
+    it 'increments the comments counter by 1' do
+      expect { @comment.comments_count }.to change { @post.comments_counter }.from(5).to(6)
+    end
+  end
 end
