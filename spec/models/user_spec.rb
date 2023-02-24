6d125798b9af1f1ac1a2ec1024ac8b758ebc295d
keebot-png <kee.bot@icloud.com>
2023-02-24 12:58:33 +0200

    Write tests for user and post and fix linters

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = User.new(name: 'Keenan Botes', bio: 'Lives in South Africa', posts_counter: 5)
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:bio) }
+
+  it { should be_valid }
+
+  describe 'when the name is not present' do
+    before { @user.name = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe '#recent_posts' do
+    it 'should have a length between 0 and 3' do
+      expect(@user.recent_posts.length).to be_between(0, 3)
+    end
+  end
+end
