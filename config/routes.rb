00fe4ce6a85177072b4d51f2c986b62436a9ddd4
keebot-png <kee.bot@icloud.com>
2023-02-27 00:02:04 +0200

    Set up post and users controllers and respective html and corrected linters

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users do
+    resources :posts, only: [:index, :show]
+  end
+  resources :users, only: [:index, :show]
 end
