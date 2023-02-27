34264e725712493700c94f8c5ae68428efbbe8c5
keebot-png <kee.bot@icloud.com>
2023-02-27 09:33:42 +0200

    Correct routes changes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :users do
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
-  resources :users, only: [:index, :show]
 end
