f18b10f57b50b3719c2c46538fd47f2db8487078
keebot-png <kee.bot@icloud.com>
2023-02-28 21:03:57 +0200

    Add layout for views and fix linters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-
   before do
     @user = User.new(name: 'Keenan Botes', bio: 'Lives in South Africa', posts_counter: 5)
   end
