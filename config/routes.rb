ac15a70d7b76e06ce3a3530c06337972038fde59
keebot-png <kee.bot@icloud.com>
2023-03-04 16:27:00 +0200

    Add root route and cleared comments.

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-
-  # get '/posts/new', to: 'posts#new', as: 'create_post'
-  # post '/posts/new', to: 'posts#create'
-  # get '/users/:user_id/posts/:id', to: 'posts#show', as:'post'
-  # post '/users/:user_id/posts/:id', to: 'posts#create_comment'
-  # post '/users/:user_id/posts/:id/likes', to: 'posts#create_like', as: 'like'
+  root "users#index"
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create, :new] do
